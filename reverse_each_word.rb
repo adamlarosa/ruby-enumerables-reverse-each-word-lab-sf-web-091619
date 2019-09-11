4764ae1db79c923b31d9b3b5216dcd7ee3ae90c7
Adam La Rosa <adam.larosa@gmail.com>
2019-09-11 22:45:56 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,16 +1,12 @@
-require 'pry'
 def reverse_each_word(sentence)
   
   array = sentence.split(/ /)
   new = []
   new_w_collect = []
   
-  array.each { |word|
-    new.push(word.reverse)
-    binding.pry
-  }
+  array.each { |word| new.push(word.reverse) }
   new.join(" ")
 
-  array.collect {|word| new_w_collect.push(word.reverse)}
+  array.collect { |word| new_w_collect.push(word.reverse) }
   new_w_collect.join(" ")
 end
